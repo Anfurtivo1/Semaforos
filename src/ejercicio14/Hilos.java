e579a5260af940d08aca3ad77a0258e46256697b
Andrés Sánchez <anfurtivo1@hotmail.com>
2020-10-19 10:40:44 +0200

    Se ha añadido la solución y se ha corregido el run

--- src/ejercicio14/Hilos.java
+++ src/ejercicio14/Hilos.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
  * @author Andres
  */
 public class Hilos extends Thread{
+    //Hay que crear un nuevo semaforo para comprobar si hay asientos, luego se usa otro para los barberos
     private static Semaphore sem= new Semaphore (3);
     int espera=0;
     public Hilos(){
@@ -20,20 +21,19 @@ public class Hilos extends Thread{
             try {
                 sem.acquire();
                 if (sem.tryAcquire()) {
-                   if (sem.tryAcquire(2, TimeUnit.SECONDS)) {
                         System.out.println("Soy el cliente "+this.getName()+" y he conseguido sitio");
                         espera=(int) (Math.random()*2000+500);
                         System.out.println("Tarda "+espera+" en cortarse el pelo");
                         System.out.println("El cliente "+this.getName()+" se ha ido");
                         sleep(espera);
                         sem.release();
-                    }
-                    else{
-                    System.out.println("El cliente "+this.getName()+" no quiso cortarse el pelo");
-                    }
+                    
                 espera=(int) Math.random()*500;
                 sleep(espera);
                 }
+                else{
+                    System.out.println("El cliente "+this.getName()+" no quiso cortarse el pelo");
+                    }
                 
             } catch (InterruptedException ex) {
                 System.out.println("Error en "+ex);
